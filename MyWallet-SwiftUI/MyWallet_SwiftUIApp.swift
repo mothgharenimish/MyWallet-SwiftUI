6214be466e707d98279c3f0b43176bb367fb6869
mothgharenimish <mothgharenimish0@gmail.com>
2025-03-14 17:21:42 +0530

    Initial Commit

---
+++ MyWallet-SwiftUI/MyWallet_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWallet_SwiftUIApp.swift
+//  MyWallet-SwiftUI
+//
+//  Created by Nimish Sharad Mothghare on 14/03/25.
+//
+
+import SwiftUI
+
+@main
+struct MyWallet_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyWallet-SwiftUITests/MyWallet_SwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  MyWallet_SwiftUITests.swift
+//  MyWallet-SwiftUITests
+//
+//  Created by Nimish Sharad Mothghare on 14/03/25.
+//
+
+import Testing
+@testable import MyWallet_SwiftUI
+
+struct MyWallet_SwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
